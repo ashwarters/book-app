3b938f047ea78d2e51f6055b8818ed537cb665b8
Ashley Warters <ashwarters@gmail.com>
2021-01-17 18:52:13 -0500

    updated components pages and utils

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,42 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 
-import { getMe, deleteBook } from '../utils/API';
+import { deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
+import { useQuery } from '@apollo/react-hooks';
+import { GET_ME } from '../utils/queries';
+
 const SavedBooks = () => {
-  const [userData, setUserData] = useState({});
+  const { loading, data } = useQuery(GET_ME);
+  const userData = data?.user || [];
+  console.log("userData:", userData);
 
-  // use this to determine if `useEffect()` hook needs to run again
-  const userDataLength = Object.keys(userData).length;
+  // if (error) return `Error! ${error.message}`;
 
-  useEffect(() => {
-    const getUserData = async () => {
-      try {
-        const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-        if (!token) {
-          return false;
-        }
-
-        const response = await getMe(token);
-
-        if (!response.ok) {
-          throw new Error('something went wrong!');
-        }
-
-        const user = await response.json();
-        setUserData(user);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-
-    getUserData();
-  }, [userDataLength]);
-
-  // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
@@ -51,19 +29,17 @@ const SavedBooks = () => {
         throw new Error('something went wrong!');
       }
 
-      const updatedUser = await response.json();
-      setUserData(updatedUser);
-      // upon success, remove book's id from localStorage
+      //remove book's id from localStorage
       removeBookId(bookId);
     } catch (err) {
       console.error(err);
     }
   };
 
-  // if data isn't here yet, say so
-  if (!userDataLength) {
-    return <h2>LOADING...</h2>;
+  if (loading) {
+    return <div>Loading...</div>
   }
+  
 
   return (
     <>
@@ -74,12 +50,12 @@ const SavedBooks = () => {
       </Jumbotron>
       <Container>
         <h2>
-          {userData.savedBooks.length
-            ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
+          {userData
+            ? `Viewing ${userData} saved ${userData === 1 ? 'book' : 'books'}:`
             : 'You have no saved books!'}
         </h2>
         <CardColumns>
-          {userData.savedBooks.map((book) => {
+          {userData.map((book) => {
             return (
               <Card key={book.bookId} border='dark'>
                 {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,27 +1,27 @@
 import gql from 'graphql-tag';
 
 export const LOGIN_USER = gql `
-  mutation login($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
-      token
-      user {
-        _id
-        username
-      }
+    mutation  login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id 
+                username
+            }
+        }
     }
-  }
 `;
 
 export const ADD_USER = gql `
-  mutation addUser($username: String!, $email: String!, $password: String!) {
-    addUser(username: $username, email: $email, password: $password) {
-      token
-      user {
-        _id
-        username
-      }
+    mutation  addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id 
+                username
+            }
+        }
     }
-  }
 `;
 
 export const SAVED_BOOK = gql `
@@ -38,6 +38,24 @@ export const SAVED_BOOK = gql `
                 title
                 image
                 link
+            }
+        }
+    }
+`;
+
+export const REMOVE_BOOK = gql `
+    mutation removeBook($bookId: ID!) {
+        removeBook(bookId: $bookId) {
+            user {
+                _id
+                username
+                bookCount
+                savedBooks {
+                    title
+                    authors
+                    bookId
+                    description
+                }
             }
         }
     }
